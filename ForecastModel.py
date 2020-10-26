46b05e53d8676ff146aa1e2bc1af65f70cecc5d5
CCsaurabh <saurabhm2795@gmail.com>
2020-10-26 20:34:17 +0530

    Update ForecastModel.py

--- ForecastModel.py
+++ ForecastModel.py
@@ -1,6 +1,4 @@
-
-
-#Autoregression Problems
+1. #Autoregression Problems
 
 # Walk throught the test data, training and predicting 1 day ahead for all the test data
 #Define df_training and df_test
@@ -18,3 +16,26 @@ for t in tqdm(range(len(df_test.col))):   # Tqdm makes you show your loop  a sma
 yhat = pd.concat(yhat)
 resultsDict['AR'] = evaluate(df_test.pollution_today, yhat.values)
 predictionsDict['AR'] = yhat.values
+
+
+
+
+2.#Drawing the Correlation Matrix
+def correlation_matrix(df):
+    from matplotlib import pyplot as plt
+    from matplotlib import cm as cm
+
+    fig = plt.figure(figsize=(16,12))
+    ax1 = fig.add_subplot(111)
+    cmap = cm.get_cmap('jet', 30)
+    cax = ax1.imshow(df.corr(), interpolation="nearest", cmap=cmap)
+    ax1.grid(True)
+    plt.title('Wine data set features correlation\n',fontsize=15)
+    labels=df.columns
+    ax1.set_xticklabels(labels,fontsize=9)
+    ax1.set_yticklabels(labels,fontsize=9)
+    # Add colorbar, make sure to specify tick locations to match desired ticklabels
+    fig.colorbar(cax, ticks=[0.1*i for i in range(-11,11)])
+    plt.show()
+
+correlation_matrix(df)
